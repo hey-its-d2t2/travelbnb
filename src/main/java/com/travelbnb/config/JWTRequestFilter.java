4d1374e3e8245a4f074178991dad56b901d4bc9e
Deepak Singh <deepsinghkumar01@gmail.com>
2024-06-20 10:52:20 +0530

    DoFilterInternal Method updated in JWTRequestFilter

--- src/main/java/com/travelbnb/config/JWTRequestFilter.java
+++ src/main/java/com/travelbnb/config/JWTRequestFilter.java
@@ -10,6 +10,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -43,6 +45,8 @@ public class JWTRequestFilter extends OncePerRequestFilter {
            if(opUserName.isPresent())  {
                AppUser appUser = opUserName.get();
                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(appUser,null, Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN")));
+               authenticationToken.setDetails(new WebAuthenticationDetails(request));
+               SecurityContextHolder.getContext().setAuthentication(authenticationToken);
            }
        }
         filterChain.doFilter(request,response);
