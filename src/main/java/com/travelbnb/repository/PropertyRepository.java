07d5baa1a60b42b9a82204585d99610c28b3f711
Deepak Singh <deepsinghkumar01@gmail.com>
2024-06-24 10:22:55 +0530

    Property Repository updated and search quer added.

--- src/main/java/com/travelbnb/repository/PropertyRepository.java
+++ src/main/java/com/travelbnb/repository/PropertyRepository.java
@@ -2,6 +2,13 @@ package com.travelbnb.repository;
 
 import com.travelbnb.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+    @Query("SELECT p FROM Property p JOIN p.location l WHERE l.name = :locationName")
+    List<Property> searchProperty(@Param("locationName") String locationName);
+
 }
