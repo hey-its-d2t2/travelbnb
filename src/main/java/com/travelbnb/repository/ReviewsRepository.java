096c05df62f03f1371b4d30f0bcb7d8aa381e634
Deepak Singh <deepsinghkumar01@gmail.com>
2024-06-27 10:02:01 +0530

    Review Repository : Updated : Find by USer Reviews Method added.

--- src/main/java/com/travelbnb/repository/ReviewsRepository.java
+++ src/main/java/com/travelbnb/repository/ReviewsRepository.java
@@ -7,7 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
     @Query("select r from Reviews r where r.appUser=:user and r.property=:property")
     Reviews findReviewsByUser(@Param("user") AppUser user, @Param("property") Property property);
+
+    @Query("select r from Reviews r where r.appUser=:user")
+   List<Reviews> findByUserReviews(@Param("user") AppUser user);
 }
