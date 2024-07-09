1c12b23b71c4f5854d510597315d9ae609bc94e3
Deepak Singh <deepsinghkumar01@gmail.com>
2024-07-09 09:47:39 +0530

    Booking Entity created.

---
+++ src/main/java/com/travelbnb/entity/Booking.java
@@ -0,0 +1,100 @@
+package com.travelbnb.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true, length = 24)
+    private String email;
+
+    @Column(name = "mobile", length = 15)
+    private String mobile;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
+    private AppUser appUser;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @Column(name = "total_nights", nullable = false)
+    private Integer totalNights;
+
+    public Integer getTotalNights() {
+        return totalNights;
+    }
+
+    public void setTotalNights(Integer totalNights) {
+        this.totalNights = totalNights;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public AppUser getAppUser() {
+        return appUser;
+    }
+
+    public void setAppUser(AppUser appUser) {
+        this.appUser = appUser;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
