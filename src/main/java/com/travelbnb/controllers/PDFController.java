9321363155830cc889c3100e742bce19d22e1c72
Deepak Singh <deepsinghkumar01@gmail.com>
2024-07-10 12:07:20 +0530

    PDF Controller Created.

---
+++ src/main/java/com/travelbnb/controllers/PDFController.java
@@ -0,0 +1,24 @@
+/*
+package com.travelbnb.controllers;
+
+import com.travelbnb.service.PDFService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/pdf")
+public class PDFController {
+    PDFService pdfService;
+
+    public PDFController(PDFService pdfService) {
+        this.pdfService = pdfService;
+    }
+
+    @PostMapping
+    public void test(){
+        pdfService.generatePDF();
+    }
+}
+
+*/
